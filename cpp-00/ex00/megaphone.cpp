e21c9a9818ded133fddc6ad6bc950a7946b9e1cb
grysha11 <grzhzahar@gmail.com>
2024-12-23 10:41:49 +0100

    Add first exercise

---
+++ cpp-00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av) {
+    if (ac < 2) {
+        std::cout << "* LOUD AND UNBEREABLE FEEDBACK NOISE *" << std::endl;
+        return 1;
+    }
+    for (size_t i = 1; av[i]; i++) {
+        for (size_t j = 0; av[i][j]; j++) {
+            if (islower(av[i][j])) {
+                std::cout << (char)toupper(av[i][j]);
+            } else {
+                std::cout << av[i][j];
+            }
+        }
+    }
+    std::cout << std::endl;
+}
