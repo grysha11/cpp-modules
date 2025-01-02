0e74876c92fe332bb605c7b92d9cabab3a016736
grysha11 <grzhzahar@gmail.com>
2025-01-02 19:53:42 +0100

    do ex02

--- cpp-01/ex02/main.cpp
+++ cpp-01/ex02/main.cpp
@@ -4,5 +4,19 @@
 int main() {
     std::string str = "HI THIS IS BRAIN";
     std::string* stringPTR = &str;
-    
+    std::string &stringREF = str;
+
+    std::cout << "\t\tMemory adresses are" << std::endl;
+
+    std::cout << "The memory adress of the string variable is:" << std::endl << &str << std::endl;
+    std::cout << "The memory adress held by stringPTR is:" << std::endl << stringPTR << std::endl;
+    std::cout << "The memory adress held by stringREF is:" << std::endl << &stringREF << std::endl;
+
+    std::cout << "\t\tThe Values are" << std::endl;
+
+    std::cout << "The value of a string is:" << std::endl << str << std::endl;
+    std::cout << "The value held by stringPTR:" << std::endl << *stringPTR << std::endl;
+    std::cout << "The value held by stringREF:" << std::endl << stringREF << std::endl;
+
+    return 0;
 }
