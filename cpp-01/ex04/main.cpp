26738e9895ef7ababcb27dbc4db34510378a61ab
grysha11 <grzhzahar@gmail.com>
2025-01-13 10:12:25 +0100

    do ex-04

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -3,33 +3,33 @@
 #include <string>
 
 int main(int ac, char **av) {
-    std::size_t len = 0;
-    while (av[2][len]) {
-        len++;
-    }
-    if (ac == 4) {
-        std::string file(av[1]);
-        std::string to_find(av[2]);
-        std::string to_replace(av[3]);
-        std::ifstream src(file);
-        std::ofstream res(file);
-        if (!src.is_open() || !res.is_open()) {
-            std::cerr << "Error: Cannot open the file" << std::endl;
-            return 1;
-        }
-        std::string line;
-        while (getline(src, line)) {
-            while (line.find(to_find)) {
-                std::size_t pos = line.find(to_find);
-                line.erase(line.find(to_find), to_find.length());
-                line.insert(pos, to_replace.begin(), to_replace.end());
-            }
-        }
-        res << line;
-    } else {
+    if (ac != 4) {
         std::cerr << "Incorrect amount of arguments" << std::endl;
         return 1;
     }
+
+    std::string resultFile(av[1]);
+    std::ifstream src(av[1]);
+    std::ofstream res(resultFile.append(".replace").c_str());
+    if (!src.is_open() || !res.is_open()) {
+        std::cerr << "Error: Cannot open the file" << std::endl;
+        return 1;
+    }
+
+    std::string to_find(av[2]);
+    std::string to_replace(av[3]);
+    std::string line;
+    while (getline(src, line)) {
+        while (line.find(to_find) != std::string::npos) {
+            std::size_t pos = line.find(to_find);
+            line.erase(pos, to_find.length());
+            line.insert(pos, to_replace);
+        }
+        res << line << "\n";
+    }
+
+    src.close();
+    res.close();
 
     return 0;
 }
