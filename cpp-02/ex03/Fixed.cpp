f2d29bbc06a594011f476e5154010899b00f1f46
grysha11 <grzhzahar@gmail.com>
2025-01-28 18:44:42 +0100

    bonus in progress cpp-02

---
+++ cpp-02/ex03/Fixed.cpp
@@ -0,0 +1,156 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+    this->_value = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) {
+    this->_value = value * (1 << _bits);
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) {
+    this->_value = roundf(value * (1 << _bits));
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) {
+    this->_value = other._value;
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Default destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator = (const Fixed& other) {
+    if (this != &other) {
+        this->_value = other._value;
+    }
+    std::cout << "Copy assignment operator called" << std::endl;
+    return *this;
+}
+
+bool Fixed::operator > (const Fixed& other) const {
+    return this->_value > other._value;
+}
+
+bool Fixed::operator >= (const Fixed& other) const {
+    return this->_value >= other._value;
+}
+
+bool Fixed::operator < (const Fixed& other) const {
+    return this->_value < other._value;
+}
+
+bool Fixed::operator <= (const Fixed& other) const {
+    return this->_value <= other._value;
+}
+
+bool Fixed::operator == (const Fixed& other) const {
+    return this->_value == other._value;
+}
+
+bool Fixed::operator != (const Fixed& other) const {
+    return this->_value != other._value;
+}
+
+Fixed Fixed::operator + (const Fixed& other) {
+    Fixed tmp(0);
+    tmp._value = this->_value + other._value;
+    return tmp;
+}
+
+Fixed Fixed::operator - (const Fixed& other) {
+    Fixed tmp(0);
+    tmp._value = this->_value - other._value;
+    return tmp;
+}
+
+Fixed Fixed::operator * (const Fixed& other) {
+    Fixed tmp(0);
+    tmp._value = (this->_value * other._value) / (1 << _bits);
+    return tmp;
+}
+
+Fixed Fixed::operator / (const Fixed& other) {
+    Fixed tmp(0);
+    tmp._value = (this->_value * (1 << _bits)) / other._value;
+    return tmp;
+}
+
+Fixed& Fixed::operator ++ () {
+    this->_value++;
+    return *this;
+}
+
+Fixed Fixed::operator ++ (int) {
+    Fixed tmp(*this);
+    this->_value++;
+    return tmp;
+}
+
+Fixed& Fixed::operator -- () {
+    this->_value--;
+    return *this;
+}
+
+Fixed Fixed::operator -- (int) {
+    Fixed tmp(*this);
+    this->_value--;
+    return tmp;
+}
+
+int Fixed::getRawBits(void) const {
+    return _value;
+}
+
+void Fixed::setRawBits(int const raw) {
+    this->_value = raw;
+}
+
+int Fixed::toInt(void) const{
+    return (this->_value) / (1 << this->_bits);
+}
+
+float Fixed::toFloat(void) const{
+    return static_cast<float>(this->_value) / (1 << this->_bits);
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+Fixed& Fixed::min(Fixed& a, Fixed& b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& other) {
+    os << other.toFloat();
+    return os;
+}
---
+++ cpp-02/ex03/Point.cpp
@@ -0,0 +1,42 @@
+#include "Point.hpp"
+
+Point::Point() : _x(0), _y(0) {
+
+}
+
+Point::Point(const Fixed x, const Fixed y) : _x(x), _y(y) {
+
+}
+
+Point::Point(const Point& other) : _x(other._x), _y(other._y) {
+
+}
+
+Point::~Point() {
+
+}
+
+Point& Point::operator = (const Point& other) {
+    return *this;
+}
+
+Fixed Point::getX(void) const {
+    return this->_x;
+}
+
+Fixed Point::getY(void) const {
+    return this->_y;
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+    Fixed as_x = point.getX() - a.getX();
+    Fixed as_y = point.getY() - a.getY();
+
+    bool s_ab = (b.getX() - a.getX()) * as_y - (b.getY() - a.getY()) * as_x > 0;
+
+    if ((c.getX() - a.getX()) * as_y - (c.getY() - a.getY()) * as_x > 0 == s_ab) 
+        return false;
+    if ((c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY())*(point.getX() - b.getX()) > 0 != s_ab) 
+        return false;
+    return true;
+}
